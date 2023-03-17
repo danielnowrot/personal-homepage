82d6911ab6567384838f9c84e8b85b4188cd6fb5
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-03-17 16:34:06 +0100

    added GlobalStyle

---
+++ src/general/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        font-family: 'Inter', sans-serif;
+        color:${({ theme }) => theme.color.mainText};
+        background-color: ${({ theme }) => theme.color.mainBackground};
+        transition: 1s;
+    };
+    *, ::after, ::before {
+        box-sizing: inherit;
+    };
+`
